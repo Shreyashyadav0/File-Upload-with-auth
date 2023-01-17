1997ab5557a5d196f36e28287c8ad774fee580f1
Shreyashyadav0 <shreyashyadav0@gmail.com>
2023-01-17 10:35:17 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require ('express');
+const app = express();
+
+// middlewares
+app.use(express.json());
+app.use(express.static('content'));
+
+const PORT = 1338;
+app.listen(PORT, () =>{
+    console.log('Server is running');
+})
