8bb1442eab67862fb10c2d98c03d1b811a3cab3e
Shreyashyadav0 <shreyashyadav0@gmail.com>
2023-01-17 20:52:50 +0530

    fifth

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,34 +1,49 @@
-const jwt =  require('jsonwebtoken');
-const isAuthenticated = async(req, res, next) =>{
-    try{
-        const authHeader = req.headers.authorization;//["Bearer","{TOKEN}"]
-
-        if(!authHeader){
-            return res.status(401).json({
-                err:"authorization header is not found"
-            })
-}
-        const token = authHeader.split(" ")[1];
-
-        if(!token){
-            return res.status(401).json({
-                err:"token not found"
-            })
-        }
-
-        const decoded = jwt.verify(token,"Message");
-        const user= await User.findOne({where:{id: decoded.user.id}});
-        if(!user){
-            return res.status(404).json({err:"user not found"}); 
-        }
-        req.user= user;
-        next();
-
-    } catch(e){
-        return res.status(500).send(e); 
+const jwt = require("jsonwebtoken");
+const SECRET = "This is our secret";
+const User = require("../models/userModel");
+const isAuthenticated = async (req, res, next) => {
+  try {
+    const authHeader = req.headers.authorization;
+    
+    if (!authHeader) {
+      return res.status(401).json({
+        err: "You must be logged in",
+      });
     }
+
+    const token = authHeader.split(" ")[1]; // This is the bearer token
+
+    if (!token) {
+      return res.status(401).json({
+        err: "No token, authorization denied",
+      });
+    }
+
+    const decoded = jwt.verify(token, "Message");
+    const user = await User.findOne({ where: { id: decoded.user.id } });
+
+    if (!user) {
+      return res.status(404).json({ err: "User not found" });
+    }
+
+    req.user = user;
+    next();
+  } catch (err) {
+    console.log(err);
+    res.status(401).json({
+      err: err.message,
+    });
+  }
 };
 
-const isSeller = async(req,res, next) =>{
-     
-}
+const isSeller = (req, res, next) => {
+  if (req.user.dataValues.isSeller) {
+    next();
+  } else {
+    res.status(401).json({
+      err: "You are not a seller",
+    });
+  }
+};
+
+module.exports = { isAuthenticated, isSeller };
--- routes/product.js
+++ routes/product.js
@@ -1,11 +1,47 @@
 const express = require("express");
-const { route } = require("./user");
-const{ isAuthenticated, isSeller}= require('../middlewares/auth');      
-
 const router = express.Router();
+const uploadContent = require("../utils/fileUpload");
+const Product = require("../models/productModel");
+const { isAuthenticated, isSeller } = require("../middlewares/auth");
 
+router.post("/create", isAuthenticated, isSeller, (req, res) => {
+  uploadContent(req, res, async (err) => {
+    if (err) {
+      return res.status(500).send({ err: err.message });
+    }
+    console.log(req.body.file, req.body.price, req.body.file);
+    if (!req.body.name || !req.body.price || !req.file) {
+      return res
+        .status(400)
+        .json({ err: "All fields should be selected - name, price, file" });
+    }
 
-router.post("/create", isAuthenticated, isSeller, (req,res)=>{
- //middlewares will be called before the function will be called just to check whether the user is authenricated or not
-})
-module.exports= router; 
+    if (isNaN(req.body.price)) {
+      return res.status(400).json({ err: "Price must be a number" });
+    }
+
+    let productDetails = {
+      name: req.body.name,
+      price: req.body.price,
+      content: req.file.path,
+    };
+    
+    const createdProduct = await Product.create(productDetails);
+
+    console.log("Created Product", createdProduct);
+    
+    return res.status(201).json({ message: "Product created" });
+  });
+});
+
+router.get("/get/all", isAuthenticated, async (_req, res) => {
+  try {
+    const products = await Product.findAll();
+    return res.status(200).json({ Products: products });
+  } catch (err) {
+    console.log(err.message);
+    return res.status(500).json({ err: err.message });
+  }
+});
+
+module.exports = router;
